dc122dfb32047c1c12db29cb410bb8d728f3cae0
Guilherme.Kochepki <gui.kochepki@gmail.com>
2023-06-05 18:07:38 -0300

    Aprendendo sobre callback functions, arrow functions e conversões entre regular functions, injeção de html via Js

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -28,10 +28,109 @@
 
 
 // returning values
+// com o uso do return é possível realizar armazenar o resultado em uma variável global.
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
 
-const calcArea = function(radius){
-    return 3.14 * radius**2;
-}
+// const area =  calcArea(5);
+// console.log(area);
 
-const area =  calcArea(5);
-console.log(area);
+// ## regular function
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
+
+// arrow function
+// const calcArea = (radius) => {
+//     return 3.14 * radius**2;
+// }
+// const calcArea = radius => 3.14 * radius**2;
+
+
+
+// const area =  calcArea(5);
+// console.log('area is: ', area);
+
+// ## practise arrow functions
+
+// const greet = function(){
+//     return 'hello, world';
+// }
+// translating function
+// const greet = () => 'hello, world!';
+
+// const result = greet();
+
+// console.log(result);
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for (let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+// const bill = (products, tax) => {
+//     let total = 0;
+//     for (let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+
+
+
+
+// const name = 'shaun';
+// // functions
+
+// const greet = () => 'hello';
+
+// let resultOne = greet();
+// console.log(resultOne);
+
+// // methods
+
+// let resultTwo = name.toUpperCase();
+// console.log(resultTwo);
+
+
+
+
+
+// callbacks & foreach
+
+// const myFunc = (callbackFunc) => {
+//     //do something
+//     let value = 50;
+//     callbackFunc(value);
+// }
+
+// myFunc(value => {
+//     // do something
+//     console.log(value);
+// })
+
+
+
+// let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+// const logPerson = (person, index) => {
+//     console.log(`${index} - hello ${person}`)
+// }
+
+// people.forEach(logPerson)
+
+
+const ul = document.querySelector('.people');   //serve para referenciar
+
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+let html = ``;
+
+people.forEach(person => html += `<li style="color: purple">${person}</li>
+`)
+
+console.log(html);
+ul.innerHTML = html;            //inserir na referência acima
